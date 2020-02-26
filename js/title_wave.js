6abca74c3225eb8f52b21330ccbd719f60878bf2
fuchsia-84 <mikitech9084@gmail.com>
2020-02-27 01:19:24 +0900

    set transition/scrolloff title wave, edit comment.

--- js/title_wave.js
+++ js/title_wave.js
@@ -1,6 +1,6 @@
 $(function() {
    $('.title_wave_logo').on('click', function() {
+    $('body').addClass('scroll_on');
     $('.title_wave_pc').addClass('title_open');
-    $('.title_wave_pc').css('display','none');
    })
 });
