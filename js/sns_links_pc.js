eb711e65687925cd5a401a73f1c13032a248dffa
fuchsia-84 <mikitech9084@gmail.com>
2020-02-27 22:17:57 +0900

    add sns_pc scripts.

---
+++ js/sns_links_pc.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('#sns_pc_button').on('click', function() {
+     $('#sns_pc_button').toggleClass('open_sns_pc');
+     $('#sns_links_pc').toggleClass('up_sns_pc');
+    })
+ });
