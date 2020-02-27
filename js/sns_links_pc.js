5341c0c06e3ffb3db9a7f3e96a500fc8ae2c52f0
fuchsia-84 <mikitech9084@gmail.com>
2020-02-27 22:29:57 +0900

    fix open_sns_pc script, styles.

--- js/sns_links_pc.js
+++ js/sns_links_pc.js
@@ -1,6 +1,5 @@
 $(function() {
     $('#sns_pc_button').on('click', function() {
-     $('#sns_pc_button').toggleClass('open_sns_pc');
-     $('#sns_links_pc').toggleClass('up_sns_pc');
+     $('.sns_pc').toggleClass('open_sns_pc');
     })
  });
