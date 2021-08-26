0b44a388c01e8e0448569b59e9fd14c53197a350
fuchsia-84 <mikitech9084@gmail.com>
2021-08-26 12:51:05 +0900

    add title and edit comment out.

--- js/title_wave.js
+++ js/title_wave.js
@@ -3,4 +3,7 @@ $(function() {
     $('body').addClass('scroll_on');
     $('.title_wave_pc').addClass('title_open');
    })
+
+   /* ToDo : リロードしたとき、openしてたらremove。*/
+   /* ToDo : リサイズしたとき、openしてたらremove。*/
 });
