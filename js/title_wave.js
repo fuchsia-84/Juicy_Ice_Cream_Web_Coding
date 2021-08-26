bcb2e5935c1d9dad0d9e663ded2fdbee5d1df12e
fuchsia-84 <mikitech9084@gmail.com>
2021-08-26 13:39:18 +0900

    add hover to title logo button.

--- js/title_wave.js
+++ js/title_wave.js
@@ -1,6 +1,16 @@
 $(function() {
-   $('.title_wave_logo').on('click', function() {
+   $('.title_img').on('click', function() {
     $('body').addClass('scroll_on');
     $('.title_wave_pc').addClass('title_open');
-   })
+   });
+
+   $('.title_img').on('mouseover', function() {
+      console.log('のったよ');
+      $('.title_wave_pc .bg_black_25').css('display', 'block');
+   });
+
+   $('.title_img').on('mouseleave', function() {
+      $('.title_wave_pc .bg_black_25').css('display', 'none');
+   });
+
 });
