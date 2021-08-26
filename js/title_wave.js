c884ccea74b91b496d8ee1e9aaa878ac68be2437
fuchsia-84 <mikitech9084@gmail.com>
2021-08-26 12:54:16 +0900

    delete comment out.

--- js/title_wave.js
+++ js/title_wave.js
@@ -3,7 +3,4 @@ $(function() {
     $('body').addClass('scroll_on');
     $('.title_wave_pc').addClass('title_open');
    })
-
-   /* ToDo : リロードしたとき、openしてたらremove。*/
-   /* ToDo : リサイズしたとき、openしてたらremove。*/
 });
