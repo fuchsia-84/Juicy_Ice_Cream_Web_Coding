6e1df1105ae694248da7ff1b873921fabfda1209
fuchsia-84 <mikitech9084@gmail.com>
2021-08-26 12:30:48 +0900

    fix global nav menu clicked style.

--- js/nav_toggle.js
+++ js/nav_toggle.js
@@ -1,5 +1,11 @@
 $(function() {
     $('.nav_toggle_sp').on('click', function() {
      $('#header').toggleClass('open_nav');
-    })
+    });
+
+    $('#page_menu li a').on('click', function() {
+        if ( $('#header').hasClass('open_nav') ) {
+            $('#header').removeClass('open_nav');
+        }
+    });
  });
